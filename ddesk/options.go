a54e87e0b4a5436639fdbec83794f8a3d29b2d93
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2015-01-13 14:47:49 +0100

    Removes unusseful include

--- ddesk/options.go
+++ ddesk/options.go
@@ -2,9 +2,6 @@ package main
 
 import "github.com/jessevdk/go-flags"
 
-// #include <sys/file.h>
-import "C"
-
 type Options struct {
 	BuilderType   string `long:"type" short:"t" description:"Builder type for build operation, supported are client or cowbuilder" default:"client"`
 	BuilderSocket string `long:"socket" short:"s" description:"For client builder, address of the rpc server" default:"/var/lib/go-deb.builder/builder.sock"`
